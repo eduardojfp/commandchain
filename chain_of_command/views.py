572fd0f7e05bec983ec234dc0ba6db703247359a
jaked122 <jaked122@gmail.com>
2014-09-11 17:33:45 -0400

    Added the first view, which shows a group of organizations which are on the site

--- chain_of_command/views.py
+++ chain_of_command/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from chain_of_command.models import Organization
+from django.template import RequestContext, loader
 
-# Create your views here.
+
+def Organization_List(request):
+    latest_org_list = Organization.objects.all()
+    context = {'latest_org_list': latest_org_list}
+    return render(request, 'Org_list.html', context)
+    # Create your views here.
