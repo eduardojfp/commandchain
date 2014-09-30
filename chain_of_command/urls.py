69a5fc49f71aee98b889f6b0cc18ad3b7081c3e2
jaked122 <jaked122@gmail.com>
2014-09-29 20:48:52 -0400

    Added a basic form validator.

---
+++ chain_of_command/urls.py
@@ -0,0 +1,13 @@
+__author__ = 'awhite'
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.contrib.auth.views import login
+from chain_of_command import views
+from django.conf.urls.static import static
+from chaincommand import settings
+
+org_patterns = patterns(
+    url(r'^(?P<org_id>\d+)/$', views.OrganizationView),
+    url(r'^(?P<org_id>\d+)/positions/', views.position_display),
+    url(r'create/', views.create_organization)
+)
--- chaincommand/urls.py
+++ chaincommand/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.views import login
-from chain_of_command import views
+from chain_of_command import views, urls as hello
 from django.conf.urls.static import static
 from chaincommand import settings
 
@@ -12,17 +12,17 @@ urlpatterns = patterns('',
 
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^list_org/', views.Organization_List),
-                       url(r'^org/(?P<org_id>\d+)/$', views.OrganizationView,
-                           name='Org_detail'),
-                       url(r'^org/(?P<org_id>\d+)/positions',
-                           views.position_display, name='position_display'),
+                       # url(r'^org/(?P<org_id>\d+)/$', views.OrganizationView,
+                       # name='Org_detail'),
+                       # url(r'^org/(?P<org_id>\d+)/positions',
+                       #     views.position_display, name='position_display'),
                        url(r'^user/$', views.user_page,
                            name="User_page"),
                        url(r'^logout/$', views.logout_page, name="logout page"),
                        url(r'^login/$', views.login,
                            name="login"),
-                       url(r'^org/create/$', views.create_organization,
-                           name='create org'),
+                       # url(r'^org/create/$', views.create_organization,
+                       #    name='create org'),
                        url(r'^post/(?P<member_id>\d+)/create',
                            views.create_post,
                            name='create post'),
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
                            name='view post'),
                        (r'^ckeditor/', include('ckeditor.urls')),
                        url(r'^post/(?P<post_id>\d+)/edit$', views.edit_post,
-                           name='edit post')
+                           name='edit post'),
+                       url(r'^org/', include(hello.org_patterns))
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
